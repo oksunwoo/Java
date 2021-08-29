b9043594e9283e3b69a4ac0c5c7c1088c2ac42ed
oksunwoo <oksunwo@naver.com>
2021-08-29 20:30:45 +0900

    프로그래머스 / 가운데 글자 가져오기

---
+++ Programmers/Center.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int idx = s.length()/2;
+        if(s.length()%2==0){
+            answer = s.substring(idx-1,idx);
+        }else {
+            answer = s.substring(idx-1,idx);
+        }
+
+
+
+
+        return answer;
+    }
+}
+
+
+
+
+
+public class Center {
+}

198c2dbbe4c6552949fa1f3ad312effa8fac5b97
oksunwoo <oksunwo@naver.com>
2021-08-29 20:39:00 +0900

    프로그래머스 / 완주하지 못한 선수

---
+++ Programmers/Race.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        int i;
+        for(i=0; i<completion.length;i++){
+            if(participant[i].equals(completion[i])==false){
+                return  participant[i];
+            }
+        }
+
+
+
+        return participant[i];
+    }
+}
+
+
+
+
+public class Race {
+}
